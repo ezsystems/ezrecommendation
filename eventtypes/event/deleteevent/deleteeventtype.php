48ebd44894b760df00e65faeaae06194bd2cc0c9
Konstantinos Strigkos <k.strigkos@e-mediad.de>
2011-09-15 10:55:18 +0200

    delete event added

---
+++ eventtypes/event/deleteevent/deleteeventtype.php
@@ -0,0 +1,77 @@
+<?php
+
+include_once( 'kernel/classes/ezworkflowtype.php' );
+
+
+/**
+ * Class definition of the custom event called
+ * "My first event".
+ */
+class deleteEventType extends eZWorkflowEventType
+{
+    const EZ_WORKFLOW_TYPE_deleteEVENT = "deleteevent";
+
+    /**
+     * Constructor of this class
+     */
+    function deleteEventType()
+    {
+        // Human readable name of the event displayed in admin interface
+        $this->eZWorkflowEventType( deleteEventType::EZ_WORKFLOW_TYPE_deleteEVENT, "ezyoochoose delete event" );
+    }
+
+    function execute( $process, $event )
+    {
+    	
+    	//print_r($process);
+    	
+		
+    	$parameters = $process->attribute( 'parameter_list' );
+    	
+    	if ($parameters['move_to_trash'] === 1){
+    		
+    		return eZWorkflowType::STATUS_ACCEPTED;
+    		
+    	}else{
+    	
+	    	$node_list = $parameters['node_id_list'];
+	    	
+	    	foreach ($node_list as $node){
+
+	    		$node_obj = eZContentObjectTreeNode::fetch($node);
+	    		if ($node_obj){
+	    			$obj = $node_obj->ContentObject;
+	    			$class_id = $obj->ClassID;
+	    			
+	    			$ycitemtypeid = '';
+	
+					$arr = eZRecommendationClassAttribute::fetchClassAttributeList($class_id);
+					
+					if (count($arr['result']) > 0){
+
+						$ycitemtypeid = $arr['result']['ycItemType'];
+						if (!empty($ycitemtypeid)){
+	    			
+	    					$path = $ycitemtypeid.'/'.$node;
+    				        eZDebug::writeDebug('ezyoochoose delete event executed.');
+	    					ezYCFunctions::delete_item_request($path);
+
+						}
+						
+					}
+				
+	    		}
+	    	
+	    	}
+	    		    	
+
+	        
+    	}
+        return eZWorkflowType::STATUS_ACCEPTED;
+    }
+
+}
+
+eZWorkflowEventType::registerEventType( deleteEventType::EZ_WORKFLOW_TYPE_deleteEVENT, "deleteEventType" );
+
+?>
