6ade80db9b639bcc58da23bf0899b4b41ffe3c0b
Bertrand Dunogier <bd@ez.no>
2012-05-15 18:14:59 +0200

    Added missing API Exception class

---
+++ classes/api_exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File containing the eZRecommendationException class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ */
+
+class eZRecommendationApiException extends Exception
+{
+    public function __construct( $message )
+    {
+        $this->message = $message;
+    }
+}
+?>
