a8837fde91cfa701876657569e25e4960725c821
Bertrand Dunogier <bd@ez.no>
2012-05-16 17:39:51 +0200

    Added missing header tags

--- classes/api.php
+++ classes/api.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+/**
+ * High level class that provides interaction with the recommendation services
+ */
 class eZRecommendationApi
 {
     public function __construct()
--- classes/struct/api_get_recommendations.php
+++ classes/struct/api_get_recommendations.php
@@ -4,7 +4,8 @@
  *
  * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
-
+ * @version //autogentag//
+ */
 /**
  * This struct is used to send a recommendation API request
  */
