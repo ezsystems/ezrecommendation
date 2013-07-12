ezsystems/ezrecommendation

commit 6c5a5b89b9d4b4395543f851754ac54459faf36e

Fixed EZP-20261: load scenario list dynamically from the yoochoose server

classes/ezrecommendationserverapi.php
@@ -11,7 +11,7 @@
 /**
  * High level class that provides interaction with the recommendation services
  */
-class eZRecommendationApi
+class eZRecommendationServerAPI
 {
     public function __construct()
     {
@@ -374,6 +374,57 @@ class eZRecommendationApi
 
         return true;
     }
+
+    /**
+     * Returns the list of configured scenarii on the yoochoose server
+     *
+     * @return array an array, indexed by scenario ID, of hashes with 3 keys: id, title and description
+     */
+    public static function getScenarioList()
+    {
+        $ini = eZINI::instance( 'ezrecommendation.ini' );
+
+        $url = sprintf(
+            'https://%s/restfrontend/ebl/v3/%d/structure/get_scenario_list',
+            $ini->variable( 'URLSettings', 'ConfigURL' ),
+            $ini->variable( 'ClientIdSettings', 'CustomerID' )
+        );
+        $request = new ezpHttpRequest( $url );
+        $request->setOptions(
+            array(
+                'httpauthtype' => HTTP_AUTH_BASIC,
+                'httpauth' => $ini->variable( 'ClientIdSettings', 'CustomerID' ) . ':' . $ini->variable( 'ClientIdSettings', 'LicenseKey' )
+            )
+        );
+        try
+        {
+            $response = $request->send();
+        }
+        catch ( HttpRuntimeException $e )
+        {
+            eZDebugSetting::writeError( 'extensionezrecommendation', $e->getMessage() );
+        }
+
+        $rawScenarioList = json_decode( $response->getBody() );
+        if ( !is_object( $rawScenarioList ) || !count( $rawScenarioList->scenarioInfoList ) )
+            return array();
+
+        $scenarioList = array();
+        foreach ( $rawScenarioList->scenarioInfoList as $rawScenario )
+        {
+            if ( $rawScenario->enabled != 'ENABLED' )
+                continue;
+
+            $scenarioList[$rawScenario->referenceCode] = array(
+                'id' => $rawScenario->referenceCode,
+                'title' => $rawScenario->title . " from server",
+                'description' => $rawScenario->description
+            );
+        }
+
+        return $scenarioList;
+    }
+
     /**
      * Instance of ezrecommendation.ini
      * @var eZINI

classes/ezrecommendationapi.php
@@ -1,0 +1,54 @@
+<?php
+/**
+ * File containing the ${NAME} class.
+ *
+ * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+class eZRecommendationAPI
+{
+    private $cacheManager;
+
+    const CACHE_KEY = 'ezrecommendation-cache';
+
+    public function __construct( eZRecommendationCacheManager $cacheManager = null )
+    {
+        if ( !isset( $cacheManager ) )
+        {
+            $this->cacheManager = new eZRecommendationCacheManager;
+        }
+    }
+
+    public function getScenarioList()
+    {
+        $cacheFilePath = $this->getCacheFilePath( 'scenario_list' );
+        if ( $data = $this->cacheManager->getFromCache( $cacheFilePath, self::CACHE_KEY ) )
+        {
+            eZDebugSetting::writeDebug( 'extension-ezrecommendation', "Loaded from cache", "Load scenario list" );
+            return $data;
+        }
+
+        $scenarioList = eZRecommendationServerAPI::getScenarioList();
+        $this->cacheManager->storeCache( $cacheFilePath, self::CACHE_KEY, $scenarioList );
+        eZDebugSetting::writeDebug( 'extension-ezrecommendation', "Loaded from server", "Load scenario list" );
+
+        return $scenarioList;
+    }
+
+    /**
+     * Returns the path for the cache file of data identified by $identifier
+     * @param string $identifier
+     *
+     * @return string
+     */
+    protected function getCacheFilePath( $identifier )
+    {
+        return sprintf(
+            '%s/ezrecommendation/%s.php',
+            eZSys::cacheDirectory(),
+            $identifier
+        );
+    }
+}

classes/ezrecommendationcachemanager.php
@@ -1,0 +1,46 @@
+<?php
+/**
+ * File containing the ezrecommendationcachemanager class.
+ *
+ * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+class eZRecommendationCacheManager
+{
+    public function getFromCache( $path, $key )
+    {
+        $expiryHandler = eZExpiryHandler::instance();
+
+        if ( !$expiryHandler->hasTimestamp( $key ) )
+        {
+            return false;
+        }
+
+        if ( !file_exists( $path ) || filemtime( $path ) < $expiryHandler->timestamp( $key ) )
+        {
+            return false;
+        }
+
+        return include( $path );
+    }
+
+    public function storeCache( $path, $key, $data )
+    {
+        if ( !is_array( $data ) )
+        {
+            throw new InvalidArgumentException( "\$data argument must be a valid array" );
+        }
+
+        $dataString = "<" . "?php\nreturn ". var_export( $data, true ) . ";\n?" . ">\n";
+        if ( !eZFile::create( basename( $path ), dirname( $path ), $dataString ) )
+        {
+            throw new InvalidArgumentException( "Unable to open cache file $path for writing" );
+        }
+
+        $expiryHandler = eZExpiryHandler::instance();
+        eZExpiryHandler::registerShutdownFunction();
+        $expiryHandler->setTimestamp( $key, time() );
+    }
+}

classes/ezrecommendationclusterprovider.php
@@ -1,0 +1,18 @@
+<?php
+/**
+ * File containing the eZRecommendationClusterProvider class.
+ *
+ * @copyright Copyright (C) 2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+class eZRecommendationClusterProvider
+{
+    /**
+     * @return eZClusterFileHandlerInterface
+     */
+    public function getInstance( $path = false )
+    {
+        return eZClusterFileHandler::instance( $path );
+    }
+}

classes/ezrecoserverfunctions.php
@@ -51,16 +51,20 @@ class ezRecoServerFunctions
         $requestParameters->object = $node->attribute( 'object' );
 
         // scenario argument
+        $api = new eZRecommendationAPI;
         $requestParameters->scenario = array_shift( $args );
-        $availableScenario = $recommendationIni->variable( 'BackendSettings', 'AvailableScenarios' );
-        if ( !in_array( $requestParameters->scenario, array_keys( $availableScenario ) ) )
+        $availableScenarios = $api->getScenarioList();
+        if ( !in_array( $requestParameters->scenario, array_keys( $availableScenarios ) ) )
         {
             throw new InvalidArgumentException(
                 ezpI18n::tr(
                     'extension/ezrecommendation',
-                    'Unknown scenario %scenario. Available scenarios: %available_scenarii',
+                    'Unknown scenario %scenario. Available scenarios: %available_scenarios',
                     null,
-                    array( '%scenario' => $requestParameters->scenario, '%available_scenarii' => implode( ', ', $availableScenario ) )
+                    array(
+                        '%scenario' => $requestParameters->scenario,
+                        '%available_scenarios' => implode( ', ', array_keys( $availableScenarios ) )
+                    )
                 )
             );
         }
@@ -74,7 +78,7 @@ class ezRecoServerFunctions
         $trackRenderedItems = (bool)array_shift( $args );
         $createClickRecommendedEvent = (bool)array_shift( $args );
 
-        $api = new eZRecommendationApi();
+        $api = new eZRecommendationServerAPI();
         $recommendations = $api->getRecommendations( $requestParameters );
 
         $tpl = eZTemplate::factory();

design/standard/templates/ezoe/customattributes/scenario_list.tpl
@@ -1,0 +1,5 @@
+<select name="{$custom_attribute}" id="{$custom_attribute_id}_source"{if $custom_attribute_disabled} disabled="disabled"{/if} title="{$custom_attribute_title|wash}" class="{$custom_attribute_classes|implode(' ')}">
+    {foreach fetch( 'ezrecommendation', 'scenario_list' ) as $scenario}
+        <option value="{$scenario.id|wash}"{if $scenario.id|eq( $custom_attribute_default )} selected="selected"{/if} title="{$scenario.description|wash}">{$scenario.title|wash}</option>
+    {/foreach}
+</select>

eventtypes/event/ezrecodeleteevent/ezrecodeleteeventtype.php
@@ -43,7 +43,7 @@ class eZRecoDeleteEventType extends eZWorkflowEventType
         }
         else
         {
-            $api = new eZRecommendationApi();
+            $api = new eZRecommendationServerAPI();
             foreach ( $parameters['node_id_list'] as $nodeID )
             {
                 $api->deleteItem( $nodeID );

eventtypes/event/ezrecoexportevent/ezrecoexporteventtype.php
@@ -33,7 +33,7 @@ class eZRecoExportEventType extends eZWorkflowEventType
 
         $objectID = $processParameters['object_id'];
 
-        $api = new eZRecommendationApi();
+        $api = new eZRecommendationServerAPI();
         $api->exportObject( $objectID );
         return eZWorkflowType::STATUS_ACCEPTED;
     }

commit b76414598859aca7df249ca1bbf30498de7757e2

Fixed a string error in EZP-20261
Introduced in 6c5a5b89b9d4b4395543f851754ac54459faf36e

classes/ezrecommendationserverapi.php
@@ -417,7 +417,7 @@ class eZRecommendationServerAPI
 
             $scenarioList[$rawScenario->referenceCode] = array(
                 'id' => $rawScenario->referenceCode,
-                'title' => $rawScenario->title . " from server",
+                'title' => $rawScenario->title,
                 'description' => $rawScenario->description
             );
         }
@@ -431,4 +431,3 @@ class eZRecommendationServerAPI
      */
     private $ini;
 }
-?>